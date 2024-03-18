6c33ea44e178cd53af5e551b04f8afcd8690e071
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-03-18 14:48:03 +0000

    u

--- js/script.js
+++ js/script.js
@@ -10,12 +10,17 @@ function doMathClicked () {
   document.getElementById('subtract-math').innerHTML = '7 - 3 =  ' + (7 - 3)
   document.getElementById('multiply-math').innerHTML = '3 + 4 * 2 = ' + (3 + 4 * 2)
   document.getElementById('divide-math').innerHTML = '(4 / 2) + 3 = ' + ((4 / 2) + 3)
-  document.getElementById('exponent-math').innerHTML = '5 + 2 ** 3 = ' + (5 + 2 ** 3)
+  document.getElementById('exponent-math').innerHTML = '5 + 2³ = ' + (5 + 2 ** 3)
   document.getElementById('square-root-math').innerHTML = '√(81) = ' + Math.sqrt(81)
 }
 
-
+// This function calculates the area of a rectangle
 function area(length, width) {
   var result = parseInt(length) * parseInt(width);
-  document.getElementById("output").innerHTML = result;
+  document.getElementById("output").innerHTML = result + "m<sup>2</sup>";
+  
+}// This function calculates the perimeter of a rectangle 
+function perimeter(length, width) {
+  var result = parseInt(2*length) + parseInt(2*width);
+  document.getElementById("output2").innerHTML = result + "m";
 }
